8ca55b62c849d87ae416a9a0520f042891aaea42
AyselEmrahli <aysel.e@code.edu.az>
2016-07-03 12:37:43 +0400

    :/

--- shop_sidebar.php
+++ shop_sidebar.php
@@ -22,5 +22,221 @@
     <![endif]-->
   </head>
   <body>
+
+     <!-- Shop SideBar page start -->
+
+    <!-- green blog start-->
+<section class="shopSidebar ">
+
+    <div class="blog">
+        <div class="shopSidebarBg"></div>
+        <img src="images/_slider2.jpg">
+        <h4 style="color: white; font-weight: bold;">SHOP</h4>
+    </div>
+    <!-- green blog end -->
+
+    <div class="shopBottom">
+     <!-- Page location start -->
+        <div class="container-fluid">
+            <div class="container labelPart">
+              <h5>home</h5>
+              <h5 class="fa fa-angle-right" aria-hidden="true"></h5>
+              <h5>about us</h5>
+            </div>
+        </div>
+     <!-- Page Location end -->
+    </div>
+
+    <hr>
+
+    <div class="container">
+     <!-- Showing Results line start -->   
+            <div class="showingResults">
+              <h5>SHOWING 1-9 OF 100 RESULTS</h5>
+               <H6 class="dropdown">
+                  <p href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: black;">DEFAULT SORTING <span class="glyphicon glyphicon-menu-down"></span></p>
+                  <ul class="dropdown-menu dropOrg">
+                    <li>First</li>
+                    <li>Second</li>
+                    <li>Third</li>
+                  </ul>
+                </H6>
+            </div>
+
+            
+     
+     <!-- Showing Results line end -->
+    </div>
+
+    <div class="container">  <hr>
+     
+        <div class="row">
+
+
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          <div class=" col-xs-11 col-sm-6 col-md-12 col-lg-12">
+            <div class="filter">
+                <h3 class="text-center">FILTER</h3>
+                <progress class="text-center progress progress-success" value="75" max="100">75%</progress>
+                <span class="pull-md-left pull-lg-left">PRICE: 12$-100$</span>
+                <button type="button" class="btn btn-secondary">Filter</button>
+            </div>
+          </div>
+
+          <div class=" col-xs-11 col-sm-6 col-md-12 col-lg-12">
+              <div class="social">
+                  <ul>
+                      <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+                  </ul>
+                 <div class="input-group col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    <input type="text" class="form-control col-xs-12 col-sm-12 col-md-12 col-lg-12" placeholder="Search">
+                </div>
+              </div>
+          </div>
+         
+          <div class=" col-xs-11 col-sm-8 col-md-12 col-lg-12">
+            <div class="category">
+              <h2 class="text-center">Categories</h2>
+              <ul class="list-group">
+                <li class="list-group-item"><a href="#">Birthday Gifts (7)</a></li>
+                <li class="list-group-item"><a href="#">Citchen Things (7)</a></li>
+                <li class="list-group-item" id="decorArt"><a style="color:green; font-weight: bold;" href="#">&nbsp; &nbsp; Decor Art (15)</a></li>
+                <li class="list-group-item"><a href="#">Every Day (3)</a></li>
+                <li class="list-group-item"><a href="#">Furniture (6)</a></li>
+                <li class="list-group-item"><a href="#">Illumination (4)</a></li>
+                <li class="list-group-item"><a href="#">Party </a></li>
+                <li class="list-group-item"><a href="#">Romantic (3)</a></li>
+                <li class="list-group-item"><a href="#">Special Goods</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+              <div class="trinagle"></div>
+              <i class="fa fa-eye" aria-hidden="true"></i>
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="lil_arrival imgScale">
+            <span class="pull-right" id="new">NEW</span>
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+        </div>
+          
+        </div>
+        </div>
+
+      <div class="container"><hr>
+        <div class="row pages">
+          
+            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+              <div class="btn-group" role="group" aria-label="First group">
+                <button type="button" class="btn btn-secondary">1</button>
+                <button type="button" class="btn btn-secondary">2</button>
+                <button type="button" class="btn btn-secondary">3</button>
+                <button type="button" class="btn btn-secondary">5</button>
+                <button type="button" class="btn btn-secondary">6</button>
+                <a href="#"></a><button type="button" class="btn btn-default btnI"><i class="fa fa-angle-right" aria-hidden="true"></i></button></a>
+              </div>
+            </div>
+          </div>
+   
+      </div>
+</section>
+
+
+    <!-- Shop SideBar page end
+
   </body>
   </html>

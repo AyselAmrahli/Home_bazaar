693497b94e3fe8a05c38dfd7dee074210072939a
AyselEmrahli <aysel.e@code.edu.az>
2016-07-04 12:31:06 +0400

    merged

--- blog_single.php
+++ blog_single.php
@@ -43,7 +43,7 @@
       </div>
 
       <div class="row blogSnTitle">
-        <h2>Zorka Men Spring/Summer Vacation</h2>
+        <h2 class="blogPageSnTitle">Zorka Men Spring/Summer Vacation</h2>
       </div>
 
       <div class="row blogPageSnTitleIcons">
@@ -76,7 +76,7 @@
       <div class="row blogPageShare">
         <div class="col-md-7">
           <div class="col-md-2 iconDiv">
-            <h5>SHARE THIS:</h5>
+            <h5 class="blogPageSnTitle">SHARE THIS:</h5>
           </div>
           <div class="col-md-1 iconDiv"><i class="fa fa-facebook" aria-hidden="true"></i></div>
           <div class="col-md-1 iconDiv"><i class="fa fa-twitter" aria-hidden="true"></i></div>
@@ -91,7 +91,7 @@
       <div class="row blogPageMainText">
         <div class="col-md-2 text-center">
           <img src="11.jpg" alt="">
-          <h5>ANNA PANDORA</h5>
+          <h5 class="blogPageSnTitle">ANNA PANDORA</h5>
           <p>Stylist</p>
         </div>
         <div class="col-md-10 blogComm">
@@ -99,76 +99,23 @@
         </div>
       </div>
 
-      <div class="row">
-        <h3>5 Comments</h3>
-      </div>
-
       <hr>
 
-      <div class="row com">
-        <div class="col-md-1"><img src="11.jpg" alt="" style="width:60px; height: 60px"></div>
-        <div class="col-md-11 blogComm">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore architecto beatae officiis iure non aut facere velit vitae vero ratione incidunt dicta obcaecati eos quibusdam tenetur aliquid commodi odio, natus!</p>
-          <div class="pull-left"><h5>Lorem Person</h5></div>
-          <div class="pull-right"><i class="fa fa-clock-o" aria-hidden="true">
-            </i> 20 April,2016 <a href="#">REPLY</a>
-          </div>
-        </div>
-
-        <div class="col-md-offset-1 col-md-11 Comments">
-          
-          <hr>
-
-          <div class="col-md-1"><img src="11.jpg" alt="" style="width:60px; height: 60px"></div>
-          
-          <div class="col-md-11 blogComm">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque officiis ullam laudantium quis quia ut odio sunt, tempora id iusto. Amet quo fugit nihil cupiditate qui rerum, et molestias, harum.</p>
-            
-            <div class="pull-left">
-              <h5>Lorem Person</h5>
-            </div>
-            
-            <div class="pull-right"><i class="fa fa-clock-o" aria-hidden="true">
-              </i> 20 April,2016 <a href="#">REPLY</a>
-            </div>
-          </div>
-          
-          <br>
-
-          <div class="col-md-1">
-            <img src="11.jpg" alt="" style="width:60px; height: 60px">
-          </div>
-          <div class="col-md-11 blogComm">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque officiis ullam laudantium quis quia ut odio sunt, tempora id iusto. Amet quo fugit nihil cupiditate qui rerum, et molestias, harum.</p>
-            <div class="pull-left">
-              <h5>Lorem Person</h5>
-            </div>
-            <div class="pull-right"><i class="fa fa-clock-o" aria-hidden="true"></i> 20 April,2016 <a href="#">REPLY</a></div>
-          </div>
-          
-        </div>
+      <div id="fb-root"></div>
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/az_AZ/sdk.js#xfbml=1&version=v2.6";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
 
 
-
-      </div>
-      <hr>
-
-      <div class="row blogPageInputs">
-        <h4>Leave a comment</h4>
-        <br>
-        <div class="col-md-1"><img class="img-circle" src="11.jpg" alt="" style="width:60px; height: 60px"></div>
-        <div class="col-md-11" style="padding-right:0px;">
-          <form action="">
-            <textarea name="" id="" cols="30" rows="6" placeholder="Your Comment"></textarea>
-            <input type="text" placeholder="Your Name*">
-            <input type="text" placeholder="Your Email*">
-            <a class="blogCommSend" href="#">SEND US</a>
-          </form>
-        </div>
-
-      </div>
+      <div class="fb-comments" data-href="https://www.facebook.com/zuck/posts/10101828640656261" data-width="1160px" data-numposts="5"></div>
     </div>
     
+  
+  
 
 
   </body>
---
+++ wish_list.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home Bazaar</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+     <!-- Font Awes#ome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/lightslider.css" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+     <!-- Wishlist page start -->
+
+    <!-- green blog start-->
+<section class="wishList">
+
+     <div class="container-fluid">
+      <div class="row text-center">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 green_contact">
+            <h1>WISHLIST</h1>
+          </div>
+        </div>
+      </div>
+    <!-- green blog end -->
+
+   <div class="container">
+        <div class="row">
+          <div class="navbar-contact">
+            <ul class="nav navbar-nav">
+              <li><a href="index.php">home</a></li>
+              <li><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+              <li><a href="#">wishlist</a></li>
+            </ul>
+          </div>
+      </div>
+
+  
+     <!-- Showing Results line end -->
+    </div>
+
+    <div class="container">  <hr>
+     
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+            <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+              <div class="trinagle"></div>
+              <i class="fa fa-eye" aria-hidden="true"></i>
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>  
+        </div>
+    
+        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+            <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>
+           <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+             <div class="lil_arrival imgScale">
+              <div class="trinagle"></div>
+              <i class="fa fa-eye" aria-hidden="true"></i>
+                <a href="#">
+                  <img class="center-block img-responsive"  src="images/angel.jpg">
+                </a>
+                <h6 class="text-center">Small cactus variety pot<br> from France</h6>
+                <p class="text-center">$45.00</p>
+                <button type="button" class="btn btn-default btn-block btn-card">ADD TO CARD</button>
+            </div>
+          </div>  
+        </div>
+      </div>
+
+      <div class="container"><hr>
+        <div class="row pages">
+          
+            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+              <div class="btn-group" role="group" aria-label="First group">
+                <button type="button" class="btn btn-secondary">1</button>
+                <button type="button" class="btn btn-secondary">2</button>
+                <button type="button" class="btn btn-secondary">3</button>
+                <button type="button" class="btn btn-secondary">5</button>
+                <button type="button" class="btn btn-secondary">6</button>
+                <a href="#"></a><button type="button" class="btn btn-default btnI"><i class="fa fa-angle-right" aria-hidden="true"></i></button></a>
+              </div>
+            </div>
+          </div>
+   
+      </div>
+</section>
+
+
+    <!-- Wishlist page end
+  </body>
+  </html>
